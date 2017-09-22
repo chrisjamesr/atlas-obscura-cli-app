cab226b45a801d76f4a75eb46c4c4ada21536c77
chrisjamesr <chrisjames.r@gmail.com>
2017-09-22 13:27:13 -0400

    Remove scraper methods from CLI and call from object methods

--- lib/atlas_obscura_cli_app/cli.rb
+++ lib/atlas_obscura_cli_app/cli.rb
@@ -71,7 +71,7 @@ class AtlasObscuraCliApp::CLI
 		# binding.pry
 		puts country.name
 		puts "\n"
-		AtlasObscuraCliApp::Scraper.scrape_destinations(country).each_with_index do |d, index |
+		Destination.create_from_url(country).each_with_index do |d, index |
 			# binding.pry
 			puts "\n"
 			puts "#{index+1}.  #{d.name}   -    #{d.city}"
